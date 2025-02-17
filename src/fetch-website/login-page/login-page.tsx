bcbc6846ee9de9301dbf55505d3b34bc2d6c33a4
Justin <70607137+justinlogan03@users.noreply.github.com>
2025-02-17 14:14:36 -0500

    handle input state

--- src/fetch-website/login-page/login-page.tsx
+++ src/fetch-website/login-page/login-page.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import PetsIcon from "@mui/icons-material/Pets";
 import { PrimaryButton, SecondaryButton } from "../common-components/buttons";
 import { TextField } from "@mui/material";
 
 export const LoginPage = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+
   return (
-    <div className="grid p-20 gap-y-12 bg-blue-100 w-128 rounded">
+    <div className="grid py-20 px-28 gap-y-12 bg-blue-100 w-128 rounded">
       <div className="flex mx-auto text-2xl ">
         <PetsIcon fontSize="large" />
         <h1 className="font-bold ml-2 my-auto">Login</h1>
@@ -13,22 +16,38 @@ export const LoginPage = () => {
       <TextField
         required
         id="username"
-        label="Username"
-        defaultValue="Hello World"
+        label="Name"
+        defaultValue=""
         variant="filled"
         color="primary"
+        onInput={(e) => {
+          const target = e.target as HTMLTextAreaElement;
+          setName(target.value);
+        }}
+        value={name}
       />
       <TextField
         required
         id="password"
-        label="Password"
-        defaultValue="Hello World"
+        label="Email"
+        defaultValue=""
         variant="filled"
         color="primary"
+        onInput={(e) => {
+          const target = e.target as HTMLTextAreaElement;
+          setEmail(target.value);
+        }}
+        value={email}
       />
       <div className="flex gap-x-4 mx-auto">
         <PrimaryButton label={"Submit"} onClick={() => {}} />
-        <SecondaryButton label={"Reset"} onClick={() => {}} />
+        <SecondaryButton
+          label={"Reset"}
+          onClick={() => {
+            setName("");
+            setEmail("");
+          }}
+        />
       </div>
     </div>
   );
