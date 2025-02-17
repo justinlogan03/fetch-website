07e81ef653430992a2e7d369e9f0e7d42b822399
Justin <70607137+justinlogan03@users.noreply.github.com>
2025-02-17 11:45:03 -0500

    Init Commit with tailwind

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index_bundle.js",
+  },
+  target: "web",
+  devServer: {
+    port: "3000",
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    open: true,
+    hot: true,
+    liveReload: true,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".tsx", ".jpg", ".css"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader",
+        exclude: /((node_modules)|dist|graphql)/,
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        include: /graphql/,
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: "url-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+      publicPath: "/",
+    }),
+  ],
+};
